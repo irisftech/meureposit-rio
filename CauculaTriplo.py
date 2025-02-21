7c5313701978efb52c077e3b0f68626ad9943d83
Iris Ferreira <138939070+irisftech@users.noreply.github.com>
2025-02-21 17:12:57 -0300

    adicionado cauculo

--- CauculaTriplo.py
+++ CauculaTriplo.py
@@ -5,4 +5,9 @@ triplo = int(num) * 3
 print()
 print("O triplo de" , num, "é" , triplo)
 
-#treino cauculo python
+num = input("Digite um numero")
+
+quadruplo = int(num) * 4
+
+print()
+print("O quadruplo de" , num, "é" , quadruplo)
